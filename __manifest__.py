c75e9bf7ba1346e0dcbd078b9e57913715881b90
sultannaif <sulltan.naif@gmail.com>
2026-04-23 17:32:41 +0300

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Charity Onboarding',
+    'version': '1.0',
+    'category': 'Website',
+    'summary': 'Manage beneficiary onboarding through website',
+    'depends': ['base', 'website', 'portal'],
+    'data': [
+        'views/partner_view.xml',
+        'views/templates.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
